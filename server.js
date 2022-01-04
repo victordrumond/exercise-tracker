503b5439d16fcaa844ca3dabf73c18912fa5642c
victordrumond <victordrumond@hotmail.com>
2022-01-04 03:04:24 -0300

    Update code with POST requests to create new user

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+// Require dependencies
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+// Basic configuration
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS so API is remotely testable by freeCodeCamp 
+app.use(cors());
+
+// Basic routing
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+// MongoDB & Mongoose: connect to the database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+// Define schemas & models
+const userSchema = new mongoose.Schema({
+  username: String
+});
+const UserModel = mongoose.model("User", userSchema);
 
+// Body parser: parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// POST requests: Create New User
+app.post('/api/users', (req, res) => {
+  UserModel.find({ username: req.body.username }, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else if (docs.length === 0) {
+      let newUser = new UserModel({ username: req.body.username });
+      newUser.save((err, room) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json({
+            username: room.username,
+            _id: room._id.toString()
+          });
+        };
+      });
+    } else {
+      res.json("Username already in use. Please try a different one.");
+    };
+  });
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+// Listen connection on port
+const listener = app.listen(port, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
