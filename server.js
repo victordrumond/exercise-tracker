2f81367f7fde1509f74fe30039b11d46fa1cf32e
victordrumond <victordrumond@hotmail.com>
2022-01-04 21:26:47 -0300

    Update code with POST requests to add exercises

--- server.js
+++ server.js
@@ -28,7 +28,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const userSchema = new mongoose.Schema({
   username: String
 });
-const UserModel = mongoose.model("User", userSchema);
+const exerciseSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String },
+  username: { type: String }
+});
+const UserModel = mongoose.model('User', userSchema);
+const ExerciseModel = mongoose.model('Exercise', exerciseSchema);
 
 // Body parser: parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,18 +48,20 @@ app.post('/api/users', (req, res) => {
       console.log(err);
     } else if (docs.length === 0) {
       let newUser = new UserModel({ username: req.body.username });
-      newUser.save((err, room) => {
+      newUser.save((err, user) => {
         if (err) {
           console.log(err);
         } else {
           res.json({
-            username: room.username,
-            _id: room._id.toString()
+            username: user.username,
+            _id: user._id
           });
         };
       });
+    } else if (req.body.username === '') {
+      res.json('Username cannot be empty. Please try a different one.');
     } else {
-      res.json("Username already in use. Please try a different one.");
+      res.json('Username already in use. Please try a different one.');
     };
   });
 });
@@ -60,6 +70,42 @@ app.post('/api/users', (req, res) => {
 app.get('/api/users', async (req, res) => {
   const users = await UserModel.find().select('-__v');
   res.json(users);
+});
+
+// POST request: add exercise
+app.post('/api/users/:_id/exercises', (req, res) => {
+    UserModel.findById(req.body.id, (err, docs) => {
+    if (err || !docs) {
+      res.json('There was an error processing your request. Please try again.');
+    } else if (req.body.description === '') {
+      res.json('Description is required.');
+    } else if (req.body.duration === '' || isNaN(Number(req.body.duration))) {
+      res.json('Duration is required and must be a number.');
+    } else if (req.body.date !== '' && isNaN(Date.parse(req.body.date))) {
+      res.json('Date is not required but if present must be a valid JavaScript string date.');
+    } else {
+      let newExercise = new ExerciseModel({
+        username: docs.username,
+        description: req.body.description,
+        duration: parseInt(req.body.duration),
+        date: req.body.date === '' ? new Date().toDateString() : new Date(Date.parse(req.body.date)).toDateString(),
+        userId: docs._id
+      });
+      newExercise.save((err, exercise) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json({
+            username: exercise.username,
+            description: exercise.description,
+            duration: exercise.duration,
+            date: exercise.date,
+            _id: exercise.userId
+          });
+        };
+      });
+    };
+  });
 });
 
 // Listen connection on port
