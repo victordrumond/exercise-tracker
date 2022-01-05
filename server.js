25160628482237c73f3b9f83489b01e018ac3854
victordrumond <victordrumond@hotmail.com>
2022-01-05 03:17:58 -0300

    Update code to simplify POST requests

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-// Require dependencies
+// Require dependencies & basic configuration
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const generateUniqueId = require('generate-unique-id');
 require('dotenv').config();
-
-// Basic configuration
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Enable CORS so API is remotely testable by freeCodeCamp 
-app.use(cors());
-
-// Basic routing
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
 
 // MongoDB & Mongoose: connect to the database
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,40 +15,59 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Define schemas & models
 const userSchema = new mongoose.Schema({
-  username: String
+  username: String,
+  _id: String
 });
 const exerciseSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: String },
-  username: { type: String }
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+});
+const logSchema = new mongoose.Schema({
+  username: String,
+  count: Number,
+  log: Array
 });
 const UserModel = mongoose.model('User', userSchema);
 const ExerciseModel = mongoose.model('Exercise', exerciseSchema);
+const LogModel = mongoose.model('Log', logSchema);
 
-// Body parser: parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 // POST request: create new user
 app.post('/api/users', (req, res) => {
-  UserModel.find({ username: req.body.username }, (err, docs) => {
+  UserModel.find({ username: req.body.username }, (err, data) => {
     if (err) {
       console.log(err);
-    } else if (docs.length === 0) {
-      let newUser = new UserModel({ username: req.body.username });
+    } else if (req.body.username === '') {
+      res.json('Username cannot be empty. Please try a different one.');
+    } else if (data.length === 0) {
+      let newUser = new UserModel({
+        "username": req.body.username,
+        "_id": generateUniqueId({
+          length: 20,
+          useLetters: true,
+          useNumbers: true
+        })
+      });
       newUser.save((err, user) => {
         if (err) {
           console.log(err);
         } else {
           res.json({
-            username: user.username,
-            _id: user._id
+            "username": user.username,
+            "_id": user._id
           });
         };
       });
-    } else if (req.body.username === '') {
-      res.json('Username cannot be empty. Please try a different one.');
     } else {
       res.json('Username already in use. Please try a different one.');
     };
@@ -74,33 +82,40 @@ app.get('/api/users', async (req, res) => {
 
 // POST request: add exercise
 app.post('/api/users/:_id/exercises', (req, res) => {
-    UserModel.findById(req.body.id, (err, docs) => {
-    if (err || !docs) {
-      res.json('There was an error processing your request. Please try again.');
-    } else if (req.body.description === '') {
-      res.json('Description is required.');
-    } else if (req.body.duration === '' || isNaN(Number(req.body.duration))) {
-      res.json('Duration is required and must be a number.');
-    } else if (req.body.date !== '' && isNaN(Date.parse(req.body.date))) {
-      res.json('Date is not required but if present must be a valid JavaScript string date.');
+
+  const dealWithDate = (date) => {
+    if (date instanceof Date && !isNaN(date)) {
+      return new Date(date);
+    } else {
+      return new Date();
+    };
+  };
+
+  UserModel.findById(req.body.id, (err, data) => {
+
+    let description = req.body.description;
+    let duration = req.body.duration;
+    let date = dealWithDate(req.body.date);
+
+    if (err) {
+      console.log(err);
     } else {
       let newExercise = new ExerciseModel({
-        username: docs.username,
-        description: req.body.description,
-        duration: parseInt(req.body.duration),
-        date: req.body.date === '' ? new Date().toUTCString() : new Date(Date.parse(req.body.date)).toUTCString(),
-        userId: docs._id
+        "username": data.username,
+        "description": description,
+        "duration": duration,
+        "date": date.toDateString()
       });
       newExercise.save((err, exercise) => {
         if (err) {
           console.log(err);
         } else {
           res.json({
-            username: exercise.username,
-            description: exercise.description,
-            duration: exercise.duration,
-            date: exercise.date,
-            _id: exercise.userId
+            "_id": req.body.id,
+            "username": exercise.username,
+            "description": exercise.description,
+            "duration": exercise.duration,
+            "date": exercise.date
           });
         };
       });
